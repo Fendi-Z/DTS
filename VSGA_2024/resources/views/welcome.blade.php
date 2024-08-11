822c0e063f371ae38dbe3f329cbeeef26887b97a
Fendi-Z <muhammadaffandi301@gmail.com>
2024-08-11 11:48:46 +0700

    first modifikasi tampilan awal 2

--- VSGA_2024/resources/views/welcome.blade.php
+++ VSGA_2024/resources/views/welcome.blade.php
@@ -5,13 +5,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>VSGA</title>
-
-        <!-- Styles -->
-        <style>
-            body {
-                text-align: center
-            }
-        </style>
     </head>
     <body>
         <h2>Ini adalah percobaan</h2>
